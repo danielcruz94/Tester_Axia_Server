2a3ce91c2e16d7a2695016e88c1894a01483153b
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-10-20 17:04:03 -0500

    cors 3

--- index.js
+++ index.js
@@ -9,19 +9,8 @@ const clienteRoutes = require('./routes/clienteAxiaRoutes');
 
 const app = express();
 
-const corsOptions = {
-  origin: '*', // permite todos los orígenes
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // métodos permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // encabezados permitidos
-  credentials: true, // permite enviar cookies y credenciales
-  optionsSuccessStatus: 204, // el código de respuesta para las preflight requests
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Asegúrate de que OPTIONS también esté habilitado
-
-
-//app.use(cors());
+app.use(cors());          
+app.options('*', cors()); 
 app.use(express.json());
 
 connectDB();
