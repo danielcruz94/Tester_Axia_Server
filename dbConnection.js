eb7c344ee5e652b8e40bd4e9bc9b3d5e680de184
danielcruz94 <daniel94cruz@gmail.com>
2025-10-23 20:57:33 -0500

    fix:cambio a varibale entorno en vercel

--- dbConnection.js
+++ dbConnection.js
@@ -1,17 +1,27 @@
+
+require('dotenv').config(); 
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', false);
-const password ='1234'
+// const password ='1234'
 
 //const connectionString=`mongodb+srv://daniel94cruz:${password}@cluster0.ecmhoaq.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
-const connectionString=`mongodb+srv://AXIAFINANZAS:${password}@cluster0.rofpd.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
-
-const connectDB=async() => {
-   await mongoose.connect(connectionString)
-.then(() => {
-    console.log('dataBase Conected')
-}).catch((error) =>{
-    console.log(error)
-} )
+// const connectionString=`mongodb+srv://AXIAFINANZAS:${password}@cluster0.rofpd.mongodb.net/Axia?retryWrites=true&w=majority&appName=Cluster0`
+const connectDB = async () => {
+    const connectionString = process.env.MONGODB_URI;
+  
+    if (!connectionString) {
+      console.error('❌ No se encontró la variable MONGODB_URI');
+      return;
+    }
+  
+    try {
+      await mongoose.connect(connectionString, {
+        serverSelectionTimeoutMS: 10000,
+      });
+      console.log('✅ MongoDB conectado correctamente');
+    } catch (error) {
+      console.error('❌ Error al conectar MongoDB:', error.message);
+    }
 }
 
 
